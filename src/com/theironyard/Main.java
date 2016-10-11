57046aa1b79135aee98c71c2ce73cd5cbbfb162f
Steven Burris <stevenburris.jr@gmail.com>
2016-10-11 17:50:09 -0400

    Read file method added;

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,8 +1,29 @@
 package com.theironyard;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+
+    static final String PEOPLE = "People.txt";
 
     public static void main(String[] args) {
 
+    }
+
+    public static ArrayList<Person> readFile() throws FileNotFoundException {
+        ArrayList<Person> people = new ArrayList<>();
+        File file = new File(PEOPLE);
+        Scanner fileScanner = null;
+        fileScanner = new Scanner(file);
+        while (fileScanner.hasNext()) {
+            String line = fileScanner.nextLine();
+            String[] columns = line.split("\\,");
+            Person person = new Person(Integer.valueOf(columns[0]),columns[1],columns[2],columns[3],columns[4],columns[5]);
+            people.add(person);
+        }
+        return people;
     }
 }
