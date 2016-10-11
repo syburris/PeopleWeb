8caf8d6514377eb199995e5af8b60fb6dee9e467
Steven Burris <stevenburris.jr@gmail.com>
2016-10-11 15:53:36 -0400

    Initial import;

---
+++ src/com/theironyard/Person.java
@@ -0,0 +1,73 @@
+package com.theironyard;
+
+/**
+ * Created by stevenburris on 10/11/16.
+ */
+public class Person {
+    int id;
+    String firstName;
+    String lastName;
+    String email;
+    String country;
+    String ip;
+
+    public Person() {
+    }
+
+    public Person(int id, String firstName, String lastName, String email, String country, String ip) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.country = country;
+        this.ip = ip;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+}
